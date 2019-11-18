84e4247dbb8bf20eefcda0c42ceb1d9266297fea
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2019-11-18 17:31:31 +0200

    Add button props from material-ui to custom buttons.

--- src/components/common/button/button-filled.tsx
+++ src/components/common/button/button-filled.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import { ButtonProps } from "@material-ui/core/Button";
 import { createStyles, withStyles } from "@material-ui/core";
-
-interface ButtonProps {
-    onClick: () => void;
-    startIcon?: React.ReactNode;
-}
 
 const StyledButton = withStyles(() =>
     createStyles({
