dfb93f40b3a74aae8e9c641245e7c32114fe5fe0
Samuli Asmala <samuli.asmala@gmail.com>
2019-11-21 23:14:39 +0200

    Update homepage

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -25,19 +25,29 @@ const Home: React.FC<RouteComponentProps> = (props) => {
                 <Grid item xs={12}>
                     <Panel.Body>
                         <Typography>
-                            Vaccination eRecord is a service that saves all your vaccination
+                            Vaccination eRecord is a free service that saves all your vaccination
                             information in one place to make them always available to you.
                         </Typography>
-                        <Typography>The services we offer include:</Typography>
+                        <Typography>The services we offer includes:</Typography>
                         <ul>
                             <li>A summary of your vaccines in an easy to print format</li>
-                            <li>Email reminders when a vaccine is due for a booster</li>
+                            <li>Check your vaccine coverage when travelling</li>
+                            <li>Subscribe for email reminders when a vaccine is due for a booster</li>
+                            <li>Pre-defined common vaccines for easy addition of new vaccines</li>
                         </ul>
                         <Typography>
                             We do not store any personal information on our site. In fact, all you
-                            need to register is an email address an a password.
+                            need to register is an email address and a password.
                         </Typography>
-                        <Typography>TODO: other content</Typography>
+                        <br/>
+                        <Typography>
+                            Upcoming features
+                        </Typography>
+                        <ul>
+                            <li>Manage several profiles (e.g. add your children)</li>
+                            <li>Get personalized vaccination requirements by destination country</li>
+                            <li>Automatic vaccine proposals based on age and Finland’s vaccination program info</li>
+                        </ul>
                         <br />
                         <br />
                         <FilledButton
