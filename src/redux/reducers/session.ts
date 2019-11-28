628dab80489a0b3b0a2b7f066e0200861cee8243
Samuli Asmala <samuli.asmala@gmail.com>
2019-11-28 11:27:42 +0200

    Fix wrong destructuring order
    Previously the old state value override the payload

--- src/redux/reducers/session.ts
+++ src/redux/reducers/session.ts
@@ -13,11 +13,11 @@ export function sessionReducer(state = _nullSession, action: SessionActionTypes)
     switch (action.type) {
         case ACTION_TYPES.RECEIVE_CURRENT_USER:
             return {
+                ...state,
                 //eslint-disable-next-line @typescript-eslint/ban-ts-ignore
                 //@ts-ignore
                 // TODO leave above ignore comment if this compiles and works with no issues
-                ...action.payload,
-                ...state
+                ...action.payload
             };
         case ACTION_TYPES.LOGOUT_CURRENT_USER:
             return _nullSession;
